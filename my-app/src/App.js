27be398d2f9c9bc4ab9fce323c5080b9ed14a1ff
lixuejiang <642898959@qq.com>
2018-03-10 21:00:02 +0800

    :fire: 定时任务 && 发送邮件

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,55 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import bodyOne from './components/js/bodyIndex1'
+// import {Router,Route,hashHistory} from 'react-router'
 import ComponentHeader from './components/js/header'
-import Cbodyone from './components/js/bodyIndex1'
-import { Button,Input } from 'antd';
+import footer from './components/js/footer'
+import ComponentDetails from './components/js/details'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
-class App extends Component {
-  componentWillMount(){
-		//定义你的逻辑即可
-		console.log("App - componentWillMount");
-	}
-
-	componentDidMount(){
-		console.log("App - componentDidMount");
-	}
-
-  render() {
-    let userid = 123456;
-    let username = "nick";
-    return (
-      <div className="App">
-
-        <ComponentHeader/>
-
-        <Cbodyone userid={userid} username={username}/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          VS VUE
-        </p>
+export default class Root extends Component{
+  render(){
+    return(
+      <Router>
         <div>
-          <Button type="primary">Primary</Button>
-          <Button>Default</Button>
-          <Button type="dashed">Dashed</Button>
-          <Button type="danger">Danger</Button>
+          <ComponentHeader/>
+          <Route component={bodyOne} exact path="/">
+          </Route>
+          <Route component={ComponentDetails} path="/details"></Route>
+          <Route component={footer} path="/list"></Route>
         </div>
-        <div className="example-input">
-          <Input size="large" placeholder="large size" />
-          <Input placeholder="default size" />
-          <Input size="small" placeholder="small size" />
-        </div>
-        <div>
-          {this.props.children}
-				</div>
-
-      </div>
-    );
+      </Router>
+    )
   }
-}
 
-export default App;
+}
