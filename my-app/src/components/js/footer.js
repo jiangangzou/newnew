ad052ef240c2dda6879409f9094e823884f6fb8c
yyccQQu <643929860@qq.com>
2018-03-11 15:11:25 +0800

    add

--- my-app/src/components/js/footer.js
+++ my-app/src/components/js/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 //var footerCss  =  require("../../css/footer.css");
+import "../css/style.css"
 export default class ComponentFooter extends React.Component {
 	render() {
 		var footerConvertStyle = {
@@ -18,6 +19,8 @@ export default class ComponentFooter extends React.Component {
 		return (
 			<footer style={footerConvertStyle.miniFooter}>
 				<h1 style={footerConvertStyle.miniFooter_h1}>这里是页脚，一般放置版权的一些信息。</h1>
+				{/* yarn add babel-plugin-react-html-attrs --save */}
+				<h2 class="mall">只用class</h2>
 			</footer>
 		)
 	}
--- my-app/src/components/root.js
+++ my-app/src/components/root.js
@@ -1,26 +1,26 @@
-import React,{Component} from 'react'
-import ReactDOM from 'react-dom'
-import bodyOne from './js/bodyIndex1'
-import bodyOneChild from './js/bodyOneChild'
-// import {Router,Route,hashHistory} from 'react-router'
-import footer from './js/footer'
-import ComponentDetails from './js/details'
-import {
-  Router,
-  Route
-} from 'react-router-dom'
-
-export default class Root extends Component{
-  render(){
-    return(
-      <Router>
-        <Route component={bodyOne} path="/">
-          <Route component={ComponentDetails} path="details"></Route>
-        </Route>
-        <Route component={footer} path="list"></Route>
-
-      </Router>
-    )
-  }
-
-}
+// import React,{Component} from 'react'
+// import ReactDOM from 'react-dom'
+// import bodyOne from './js/bodyIndex1'
+// import bodyOneChild from './js/bodyOneChild'
+// // import {Router,Route,hashHistory} from 'react-router'
+// import footer from './js/footer'
+// import ComponentDetails from './js/details'
+// import {
+//   Router,
+//   Route
+// } from 'react-router-dom'
+//
+// export default class Root extends Component{
+//   render(){
+//     return(
+//       <Router>
+//         <Route component={bodyOne} path="/">
+//           <Route component={ComponentDetails} path="details"></Route>
+//         </Route>
+//         <Route component={footer} path="list"></Route>
+//
+//       </Router>
+//     )
+//   }
+//
+// }
